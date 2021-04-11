025bf4165984146abdbed02ebca0b49f9d9e9bbf
David Roncancio <david@roncancio.me>
2021-04-11 11:10:47 -0400

    wip: add useFetch, add react-markdown, add json structure for posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
-import React from 'react';
+import React from 'react'
+import ReactMarkdown from 'react-markdown'
 import './App.css';
 
+import useFetch from 'use-http';
+
 function App() {
+
+  const options = {}
+  const {
+    loading,
+    error,
+    data:scaffolding = []
+  } = useFetch('https://raw.githubusercontent.com/kuryaki/blog/master/public/posts/scaffolding.md', options, [])
+
   return (
     <div className="App">
+      {error && 'Error!'}
+      {loading && 'Loading...'}
       <h1>Blog</h1>
-      <header className="App-header">
+      <div className="App-content">
+        <div className="App-post-headline">
+          <a href="/blog/scaffolding">
+            <h2>Scaffolding</h2>
+          </a>
+          <p>{scaffolding}</p>
+          <ReactMarkdown>{scaffolding}</ReactMarkdown>
+        </div>
+      </div>
+      <header className="App-footer">
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://david.roncancio.me"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          About me
         </a>
       </header>
     </div>
